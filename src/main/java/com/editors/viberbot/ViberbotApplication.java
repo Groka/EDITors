2f4e980120bce167050ae922d9284080cebb020f
hbelma <belmah95@outlook.com>
2017-07-19 17:47:03 +0200

    konfikugracija bota pokusaj

--- src/main/java/com/editors/viberbot/ViberbotApplication.java
+++ src/main/java/com/editors/viberbot/ViberbotApplication.java
@@ -1,13 +1,58 @@
 package com.editors.viberbot;
 
 
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.util.concurrent.Futures;
+import com.viber.bot.ViberSignatureValidator;
+import com.viber.bot.api.ViberBot;
+import com.viber.bot.message.TextMessage;
+import com.viber.bot.profile.BotProfile;
+
+@RestController
 @SpringBootApplication
-public class ViberbotApplication {
 
+public class ViberbotApplication implements ApplicationListener<ApplicationReadyEvent>  {
+
+	 @Inject
+	 private ViberBot bot;
+
+	 @Inject
+	 private ViberSignatureValidator signatureValidator;
+
+	 @Value("${application.viber-bot.webhook-url}")
+	 private String webhookUrl;
+
+	 @Override
+	    public void onApplicationEvent(ApplicationReadyEvent appReadyEvent) {
+	        try {
+	            bot.setWebhook(webhookUrl).get();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	        bot.onMessageReceived((event, message, response) -> response.send(message)); // echos everything back
+	        bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of( // send 'Hi UserName' when conversation is started
+	                new TextMessage("Hi"))));
+	    }
+
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ViberbotApplication.class, args);
 	}
+
 }
+
---
+++ src/main/java/config/ViberBotConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.viber.bot.ViberSignatureValidator;
+import com.viber.bot.api.ViberBot;
+import com.viber.bot.profile.BotProfile;
+
+@Configuration
+public class ViberBotConfig {
+	@Value("${application.viber-bot.auth-token}")
+    private String authToken;
+
+    @Value("${application.viber-bot.name}")
+    private String name;
+
+    @Value("${application.viber-bot.avatar:@null}")
+    private String avatar;
+
+    @Bean
+    ViberBot viberBot() {
+        return new ViberBot(new BotProfile(name, avatar), authToken);
+    }
+
+    @Bean
+    ViberSignatureValidator signatureValidator() {
+        return new ViberSignatureValidator(authToken);
+    } 
+}
